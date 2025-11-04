9ce354e5334daea9065335e0650de627b18914bd
Naveen pandian <npandian515@gmail.com>
2025-11-04 16:19:31 +0530

    initiated the spying.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,12 +3,14 @@ import Header from './component/Header'
 import HeroSection from './component/HeroSection'
 import Footer from './component/Footer'
 import LatestInsights from './component/LatestInsights'
+import Spying from './component/Spying'
 
 const App = () => {
   return (
     <div>
       <Header/>
       <HeroSection/>
+      <Spying/>
       <LatestInsights/>
       <Footer/>
     </div>
---
+++ frontend/src/component/Spying.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Spying = () => {
+  return (
+    <div className='bg-[#2925CC] h-[407.5px] w-[1440px]  py-16'>
+        
+        <div>
+            <div className='w-[720px] h-[125px] px -[15px]'>
+            <h2 className=' w-[559px] h-[45px] pl-[80.34px]'
+            >Is somone spying on your phone </h2>
+    
+           <p className='w-[559px] h-[45px] pl-[80.34px]'>Find out with Certo</p>
+        </div>
+           <button className='w-[268px] h-[54px] pl-[80.34px]'>Get Certo for iPhone</button>
+           <button className='w-[160px] h-[50px] pl-[80.34px]'>Get Certo for Android</button>
+
+        </div>
+    </div>
+  )
+}
+
+export default Spying
